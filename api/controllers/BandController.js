1a385f176c12e55cabe597dfacdcea87fad46560
Tomás Reynoso <tareynoso@gmail.com>
2016-11-05 12:22:01 -0300

    tarea BE02.02

--- api/controllers/BandController.js
+++ api/controllers/BandController.js
@@ -4,11 +4,26 @@ const band = require('../models/BandModel').instance;
 
 class BandController {
 
+    /* getList */
     getList (req, res) {
         band.getList()
             .then(documents => res.json(documents))
             .catch(error => res.json({error: error.message}));
     }
+
+    /** */
+	getBandByID(req, res) {
+        console.log("Se pidio una banda por el id");
+        return "{id:" + res.id+ "}";
+    }
+
+    /** */
+	getBandArtistByIDBand(req, res) {
+        console.log("Se pidio una banda por el id");
+        return "{id:" + res.id+ "}";
+    }
+
+
 }
 exports.BandController = BandController;
 exports.instance = new BandController();
